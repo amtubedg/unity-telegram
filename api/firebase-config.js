02654ed638f82a1d7dc2b923bd09a9d0b668e1e4
DB <200088465+amtubedg@users.noreply.github.com>
2025-03-28 17:21:46 +0400

    new test 3

--- api/firebase-config.js
+++ api/firebase-config.js
@@ -3,8 +3,8 @@ import { getFirestore } from "firebase-admin/firestore";
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+const app = initializeApp({
+  credential: cert(serviceAccount)
 });
 
 const db = getFirestore();
